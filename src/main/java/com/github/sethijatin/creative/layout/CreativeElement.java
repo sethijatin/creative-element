4b03b6ba496d5f9abf4e8ed8577c300706d1a38e
jsethi <jatinsethi@outlook.com>
2018-03-13 12:24:11 +0530

    updated changes to use appium's mobile element

--- src/main/java/com/github/sethijatin/creative/layout/CreativeElement.java
+++ src/main/java/com/github/sethijatin/creative/layout/CreativeElement.java
@@ -1,15 +1,22 @@
 package com.github.sethijatin.creative.layout;
 
+import io.appium.java_client.MobileElement;
 import org.openqa.selenium.WebElement;
 
 public class CreativeElement {
 
     private CyclicQuad quad;
-    private WebElement element;
+    private WebElement webElement;
+    private MobileElement mobileElement;
 
     public CreativeElement(WebElement e){
-        this.element = e;
+        this.webElement = e;
         this.quad = new CyclicQuad(e.getLocation(), e.getSize().getHeight(), e.getSize().getWidth());
+    }
+
+    public CreativeElement(MobileElement m){
+        this.mobileElement = m;
+        this.quad = new CyclicQuad(m.getLocation(), m.getSize().getHeight(), m.getSize().getWidth());
     }
 
     public CyclicQuad getCyclicQuad(){
@@ -17,21 +24,42 @@ public class CreativeElement {
     }
 
     public WebElement getElement(){
-        return this.element;
+        return this.webElement;
+    }
+
+    public MobileElement getMobileElement(){
+        return this.mobileElement;
     }
 
     public int getHeight(){
-        return this.element.getSize().getHeight();
+
+        if (this.webElement != null){
+            return this.webElement.getSize().getHeight();
+        }
+        else {
+            return this.mobileElement.getSize().getHeight();
+        }
+
     }
 
     public int getWidth(){
-        return this.element.getSize().getWidth();
+        if (this.webElement != null) {
+            return this.webElement.getSize().getWidth();
+        }
+        else {
+            return this.mobileElement.getSize().getWidth();
+        }
     }
 
     public boolean isVisible(){
-        return this.element.isDisplayed();
+        if (this.webElement != null) {
+            return this.webElement.isDisplayed();
+        }
+        else {
+            return this.mobileElement.isDisplayed();
+        }
     }
-        
+
     public Near isNear(){
 
         return new Near(this);
